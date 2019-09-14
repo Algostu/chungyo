8a6f00d4842753ab5b28fefbabd0403724e07357
rhcsky <mangusn1@gmail.com>
2019-09-14 17:30:09 +0900

    Update test_core.py

--- pose_diff/core/test_core.py
+++ pose_diff/core/test_core.py
@@ -1,5 +1,11 @@
 def test_core_function_test_1():
     assert 1 == 1
 
+
+def test_core_function_test_2():
+    assert 2 == 2
+
+
 if __name__ == "__main__":
     test_core_function_test_1()
+    test_core_function_test_2()
