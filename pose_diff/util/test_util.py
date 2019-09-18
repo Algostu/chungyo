49c4f4ee31e61bdd7a44a5769005e8921e6a2519
Hankyul <consistant1y@ajou.ac.kr>
2019-09-19 08:52:53 +0900

    [#9] Update check_accuracy + update check_accuracy function + add test_check_accuracy function + add sample for testing + write check_accuracy sdk

--- pose_diff/util/test_util.py
+++ pose_diff/util/test_util.py
@@ -1,5 +1,20 @@
-def test_util_function_test_1():
-    assert 1 == 1
+import os
+import numpy as np
+from pose_diff.util import Common
 
-if __name__ == '__main__':
-    test_util_function_test_1()
+def test_check_accuracy():
+    test_set = load_testSet(1)
+    ret_val = Common.check_accuracy(test_set, 1)
+    print("Average accuracy: ", ret_val[1])
+    assert ret_val[0] == True
+
+def load_testSet(ex_type):
+    fileloc = os.path.join('data', 'exercise.txt')
+    ex_list = []
+    with open(fileloc, 'r') as f:
+        ex_list = [line.rstrip('\n') for line in f]
+        ex_list = [ex[3:] for ex in ex_list]
+    file_name = ex_list[ex_type-1]
+    print(file_name)
+    testSet = np.load(os.path.join('data', 'testSets', file_name+'.npy'))
+    return testSet
