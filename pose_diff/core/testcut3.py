b3a10b204a2ad4f643549cb4a5363c24c9813b13
jungdaan <gong3gong@gmail.com>
2019-09-29 20:03:26 +0900

    [#12]update

--- pose_diff/core/testcut3.py
+++ pose_diff/core/testcut3.py
@@ -1,7 +1,5 @@
 from pose_diff.core.fastgraphdiff import Momentum
-
 import numpy as np
-
 moment = Momentum()
 
 class cut:
@@ -24,22 +22,29 @@ class cut:
         return avglist[i+2],back+1
 
     def compare_back(self,avglist,frontavg,front):
-
+        flag = 0
         back = 0
         front = int(front/3) #다시 원래대로 만들어주기
 
-        for i in range(front,len(avglist)-1):
-            if (avglist[i] > avglist[i + 1]+2 and avglist[i+1]<avglist[i+2] and avglist[i + 1]<=frontavg):
-                print(avglist[i], avglist[i + 1] + 2)
+        for i in range(front+10,len(avglist)-1):
+
+            if (avglist[i] == avglist[-1]):
+                print(avglist[i],avglist[-1])
                 break
+            else:
+
+                if (avglist[i] > avglist[i + 1] and avglist[i+1]<avglist[i+2] and avglist[i + 1]<=frontavg+3):
+                    print(avglist[i], avglist[i + 1] + 2)
+                    break
+
+
         #print(avglist[i+1])
+
         back = (i+1)*3
         print(back)
 
-
         print(avglist[i+1])
         return i,avglist[i+1],back
-
 
 class getCut:
     def __init__(self,frame,exercise_type,axis):
@@ -47,12 +52,26 @@ class getCut:
         exercise_type = 2
         axis = 1
         newcompare = cut()
+        cutarray = []
         avglist = moment.compare_avg(exercise_type,axis,frame)
         frontavg, self.front = newcompare.compare_front(avglist)
-        i,backavg, self.back = newcompare.compare_back(avglist, frontavg,self.front)
+        #cutarray.append(self.front)
 
-        #frontavg, self.front = newcompare.two_front(i,avglist,self.back)
-        #i, backavg, self.back = newcompare.compare_back(avglist, frontavg, self.front)
+        while(True):
+            try:
+                i, backavg, self.back = newcompare.compare_back(avglist, frontavg, self.front)
+                tmp = (self.front,self.back)
+                frontavg, self.front = newcompare.two_front(i, avglist, self.back)
+
+
+            except:
+                self.back = len(frame)
+                tmp = (self.front, self.back)
+                print(self.back)
+                break
+
+            cutarray.append(tmp)
+        print(cutarray)
 
     def get_frame_number(self):
         return self.front, self.back
@@ -60,4 +79,3 @@ class getCut:
 if __name__ == "__main__":
     frames = np.load("/Users/dani/Desktop/Projectfile/pose-difference/data/output/1-2/numpy/result.npy")
     run = getCut(frames,2,1)
-
--- pose_diff/testcutavgframe.py
+++ pose_diff/testcutavgframe.py
@@ -1,7 +1,5 @@
 from pose_diff.core.fastgraphdiff import Momentum
-
 import numpy as np
-
 moment = Momentum()
 
 class cut:
@@ -23,8 +21,11 @@ class cut:
         print(avglist[i+2])
         return avglist[i+2],back+1
 
-    def compare_back(self,avglist,frontavg,front):
+    def gap_cut(self,gap,frame):
+        print(gap)
 
+    def compare_back(self,avglist,frontavg,front,frame):
+        tmp = []
         back = 0
         front = int(front/3) #다시 원래대로 만들어주기
 
@@ -34,12 +35,24 @@ class cut:
                 break
         #print(avglist[i+1])
         back = (i+1)*3
+        gap = back - front
+        until = int(len(frame)/gap)
+        print(until)
         print(back)
 
-
         print(avglist[i+1])
-        return i,avglist[i+1],back
+        tmp.append(front)
+        tmp.append(back)
 
+        for z in range(1, until):
+            tmp.append(back+(gap*z))
+
+        print(tmp)
+        print(avglist[36])
+        print(avglist[69])
+        print(avglist[108])
+
+        return i,avglist[i+1],back
 
 class getCut:
     def __init__(self,frame,exercise_type,axis):
@@ -49,10 +62,11 @@ class getCut:
         newcompare = cut()
         avglist = moment.compare_avg(exercise_type,axis,frame)
         frontavg, self.front = newcompare.compare_front(avglist)
-        i,backavg, self.back = newcompare.compare_back(avglist, frontavg,self.front)
+        i,backavg, self.back = newcompare.compare_back(avglist, frontavg,self.front,frame)
+        gap = self.back- self.front
+        print(gap)
 
-        #frontavg, self.front = newcompare.two_front(i,avglist,self.back)
-        #i, backavg, self.back = newcompare.compare_back(avglist, frontavg, self.front)
+
 
     def get_frame_number(self):
         return self.front, self.back
@@ -60,4 +74,3 @@ class getCut:
 if __name__ == "__main__":
     frames = np.load("/Users/dani/Desktop/Projectfile/pose-difference/data/output/1-2/numpy/result.npy")
     run = getCut(frames,2,1)
-
