7d86e8cc71e798b492dc1749459f20ac9bb51704
hankyul <consistant1y@ajou.ac.kr>
2019-11-04 13:45:40 +0900

    [#16] Update html, css - Test which part should we modify

--- pose_diff/Test/core/test_method.py
+++ pose_diff/Test/core/test_method.py
@@ -1,6 +1,13 @@
 from pose_diff.core import save_docx
 
 def test_save_docx():
+    '''
+    Save document
+
+    Todo
+        1. html to PDF
+        2. html redering using beautify
+    '''
     print("save_docx testing...", end=' ')
     user_numpy_file_name = 'pose_diff/Test/core/test_data/exercise_numpy.npy'
     applied_trainer_numpy_file_name = 'pose_diff/Test/core/test_data/upgraded.npy'
